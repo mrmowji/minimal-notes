6dd4ce2f6977be238450efbae4df89e5f229a401
Mojtaba Javan <javan.mojtaba@gmail.com>
2019-01-09 20:23:12 +0330

    Initial commit

---
+++ background.js
@@ -0,0 +1,26 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({notes: ''}, function() {
+    console.log("Initialized.");
+  });
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "append-notes") {
+    appendSelectionToNotes();
+  }
+});
+
+function appendSelectionToNotes() {
+  chrome.tabs.executeScript({
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+    if (chrome.runtime.lastError !== undefined) {
+      return;
+    }
+    chrome.storage.sync.get(['notes'], function(result) {
+      var notes = (result.notes + "\n- " + selection[0]).trim();
+      chrome.storage.sync.set({notes: notes}, function() {
+      });
+    });
+  });
+}
---
+++ popup.js
@@ -0,0 +1,22 @@
+var textarea;
+var button;
+
+document.addEventListener('DOMContentLoaded',function() {
+  textarea = document.getElementById("notes-textarea");
+  button = document.getElementById("copy-button");
+
+  chrome.storage.sync.get(['notes'], function(result) {
+    textarea.innerText = result.notes;
+    textarea.scrollTop = textarea.scrollHeight;
+  });
+
+  textarea.onchange = function(event) {
+    chrome.storage.sync.set({notes: event.target.value}, function() {
+    });
+  };
+
+  button.onclick = function(event) {
+    textarea.select();
+    document.execCommand("copy");
+  };
+},false);
